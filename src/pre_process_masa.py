67b0175f053f88b00d06399bc2c5190b4327d0a1
TaigaHasegawa <taigahasegwa117@yahoo.co.jp>
2020-09-30 01:06:28 +0900

    to share with masa

--- src/pre_process_masa.py
+++ src/pre_process_masa.py
@@ -21,16 +21,18 @@ def main():
     segment_list = glob.glob(os.path.join(SEGMENT_DIR,"*.xlsx"))
 
     pl_df = extract_data_function(pl_list,num_years=15,num_cols=5)
-    segment_df = extract_data_function_for_masa_2(segment_list,num_years=16,num_cols=9)
-    df = pl_df.merge(segment_df, how="inner", on=["決算期", "ticker_code"])
     attribute_df = extract_data_function_for_masa(attribute_list, 6)
     attribute_df["株式コード（固有名コード）"] = attribute_df["株式コード（固有名コード）"].astype(str)
-    df = df.merge(attribute_df, how="left", left_on=["ticker_code"], right_on=["株式コード（固有名コード）"])
+    df = pl_df.merge(attribute_df, how="left", left_on=["ticker_code"], right_on=["株式コード（固有名コード）"])
 
     df["決算期"] = pd.to_datetime(df["決算期"], format="%Y/%m", errors="coerce")
+
+    segment_df = extract_data_function_for_masa_2(segment_list,num_years=16,num_cols=9)
     
-    file_name = "processed_data_for_masa.csv"
+    file_name = "processed_financial.csv"
     df.to_csv(os.path.join(PROCESSED_DIR, file_name), encoding='utf_8_sig', index=False)
+    file_name = "processed_segment.csv"
+    segment_df.to_csv(os.path.join(PROCESSED_DIR, file_name), encoding='utf_8_sig', index=False)
 
 if __name__ == "__main__":
     main()
